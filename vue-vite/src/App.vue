d50b12880c75a40010bf80adbf7298943833adcb
wangchuang <wangchuang.space@gmail.com>
2023-11-02 21:56:33 +0800

    [vue-vite]: 添加vue3 + vite

---
+++ vue-vite/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { HelloWorld } from './components/HelloWorld.jsx';
+</script>
+
+<template>
+  <div>
+    Hello world
+    <HelloWorld />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
