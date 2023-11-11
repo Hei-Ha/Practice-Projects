461de902d9d617ef53f308206d81bd664f46f5bd
wangchuang <wangchuang.space@gmail.com>
2023-11-11 10:42:48 +0800

    [react-admin]: 暂存

--- react-admin/src/views/Test/menu.js
+++ react-admin/src/views/Test/menu.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default [
     {
-        label: 'Test',
+        label: 'Test页面',
         key: '/Test',
         rules: ['Test'],
         weight: 2,
         children: [
             {
-                label: <Link to='/Test/Test1'>Test112</Link>,
+                label: <Link to='/Test/Test1'>React 中父组件调用子组件方法</Link>,
                 key: '/Test/Test1',
                 rules: ['*']
             }
--- react-admin/src/views/Test/src/Test1.tsx
+++ react-admin/src/views/Test/src/Test1.tsx
@@ -1,17 +1,20 @@
 import React, {useRef} from 'react'
-import { Button } from 'antd';
-import { Test2 } from './Test2';
+import {Button} from 'antd';
+import {Test2} from './Test2';
+
 export const Test1 = () => {
     const env = useRef(null);
     
     
-    
     return <div>
         Parent
-        <Button type='primary' className='text-#000000' onClick={() => {
-            env.current.childFun()
-            console.log(123)
-        }}>调用子组件方法</Button>
-        <Test2 env={env} />
+        <Button
+            type='primary'
+            className='text-#000000'
+            onClick={() => {
+                env.current.childFun()
+                console.log(123)
+            }}>调用子组件方法</Button>
+        <Test2 env={env}/>
     </div>
 }
