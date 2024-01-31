bdde3031abf727390c77fef4f932f896e97f1826
wangchuang <wangchuang.space@gmail.com>
2024-01-31 21:11:57 +0800

    升级 antd 组件版本

--- react-admin/src/index.js
+++ react-admin/src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react'
 import { App } from './App.tsx'
-import 'antd/dist/antd.css'
+import 'antd/dist/reset.css'
 import './index.less'
 import './utils/utils.less'
 import "tailwindcss/tailwind.css"
