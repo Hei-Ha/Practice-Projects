4de928199191b2f7d1c2866b59e10a98ad0b9571
wangchuang <wangchuang.space@gmail.com>
2024-06-13 20:52:59 +0800

    自定义 hooks，动态改变 url 传递的参数

--- react-admin/src/views/Test/src/Test5/index.tsx
+++ react-admin/src/views/Test/src/Test5/index.tsx
@@ -1,17 +1,32 @@
+import { Button } from 'antd'
 import React, { useState, useEffect } from 'react'
 import useCustomXY from './useCustomXY'
+import useChangeUrlParams from "@/views/Test/src/Test5/useChangeUrlParams";
 
 const CustomXy = () => {
     const [x, y] = useCustomXY()
+    
+    const [testUrlParams, setTestUrlParams] = useState<number>(123)
+    
+    const [name, setName] = useChangeUrlParams('name', 'name')
+    const [title, setTitle] = useChangeUrlParams('title', 'title')
+
+    // useEffect(() => {
+    //     console.log('x', x)
+    //     console.log('y', y)
+    // }, [x, y])
+    
+    
 
 
-    useEffect(() => {
-        console.log('x', x)
-        console.log('y', y)
-    }, [x, y])
-
-
-    return <div>123</div>
+    return <div>
+        <Button onClick={() => { setName(name + '1')}}>
+            name + 1
+        </Button>
+        <Button onClick={() => { setTitle(title + '2')}}>
+            title + 2
+        </Button>
+    </div>
 }
 
 export default CustomXy
---
+++ react-admin/src/views/Test/src/Test5/useChangeUrlParams.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+
+const useChangeUrlParams = (key: string, initvalue: string) => {
+    const [value, setValue] = useState<string>(initvalue)
+    
+    const urlArr = decodeURIComponent(window.location.href).split('?')
+    
+    const handleUrlParams = (urlParmas: string) => {
+        const map = new Map()
+        urlParmas.split('&').forEach((item: string) => {
+            let arr = item.split('=')
+            map.set(arr[0], arr[1])
+        })
+        map.set(key, value)
+        let str = new URLSearchParams(Object.fromEntries(map)).toString()
+        history.replaceState('', '', `${urlArr[0]}?${str}`)
+    }
+    
+    
+    useEffect(() => {
+        handleUrlParams(urlArr[1])
+    }, [value])
+    
+    
+    return [value, setValue] as const
+
+
+}
+
+export default useChangeUrlParams
