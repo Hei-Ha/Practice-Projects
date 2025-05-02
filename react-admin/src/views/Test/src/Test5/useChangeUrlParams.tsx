d6652308d526b7947ae76f87456b7ce39c54098d
HeiHa <heiha.space@gmail.com>
2025-05-02 19:59:19 +0800

    规整代码

--- react-admin/src/views/Test/src/Test5/useChangeUrlParams.tsx
+++ react-admin/src/views/Test/src/Test5/useChangeUrlParams.tsx
@@ -1,31 +1,25 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
 
 const useChangeUrlParams = (key: string, initvalue: string) => {
-    const [value, setValue] = useState<string>(initvalue)
-    
-    const urlArr = decodeURIComponent(window.location.href).split('?')
-    
+    const [value, setValue] = useState<string>(initvalue);
+
+    const urlArr = decodeURIComponent(window.location.href).split("?");
+
     const handleUrlParams = (urlParmas: string) => {
-        const map = new Map()
-        urlParmas?.split('&').forEach((item: string) => {
-            let arr = item.split('=')
-            map.set(arr[0], arr[1])
-        })
-        map.set(key, value)
-        let str = new URLSearchParams(Object.fromEntries(map)).toString()
-        history.replaceState('', '', `${urlArr[0]}?${str}`)
-    }
-    
-    
+        const map = new Map();
+        urlParmas?.split("&").forEach((item: string) => {
+            let arr = item.split("=");
+            map.set(arr[0], arr[1]);
+        });
+        map.set(key, value);
+        let str = new URLSearchParams(Object.fromEntries(map)).toString();
+        history.replaceState("", "", `${urlArr[0]}?${str}`);
+    };
     useEffect(() => {
-        handleUrlParams(urlArr[1])
-    }, [value])
-    
-    
-    return [value, setValue] as const
+        handleUrlParams(urlArr[1]);
+    }, [value]);
 
+    return [value, setValue] as const;
+};
 
-}
-
-export default useChangeUrlParams
+export default useChangeUrlParams;
