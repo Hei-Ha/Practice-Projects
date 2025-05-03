2cc50efe0cbcc6a8bb2c29eb328b2c2b41282e44
HeiHa <heiha.space@gmail.com>
2025-05-03 11:24:41 +0800

    添加路由

--- admin-react-vite/src/main.tsx
+++ admin-react-vite/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
 import './style/global.css'
+import router from './route/router'
+import { RouterProvider } from 'react-router'
+
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<App />)
+root.render(<RouterProvider router={router} />)
 
 
 
