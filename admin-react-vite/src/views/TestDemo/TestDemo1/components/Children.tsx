7947b6b8369205d0add242e38ed46c119100f9e1
HeiHa <heiha.space@gmail.com>
2025-05-17 15:45:49 +0800

    TestDemo1 ，父组件调用子组件方法

--- admin-react-vite/src/views/TestDemo/TestDemo1/components/Children.tsx
+++ admin-react-vite/src/views/TestDemo/TestDemo1/components/Children.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import { useImperativeHandle } from 'react';
+import { Button } from '@arco-design/web-react';
 
-export const Children = () => {
+
+interface ChildrenProps {
+    elm: React.RefObject<{ childrenFun: () => void } | null> 
+}
+export const Children = ({ elm }: ChildrenProps) => {
+
+
+
+    useImperativeHandle(elm, () => {
+        return {
+            childrenFun: handleChildrenClick
+        }
+    })
+
+    const handleChildrenClick = () => {
+        console.log('This is children function')
+    }
     return <div>
         <h1>Children</h1>
+        <Button type="primary" onClick={handleChildrenClick}>Children Fun</Button>
     </div>
 }
 
--- admin-react-vite/src/views/TestDemo/TestDemo1/index.tsx
+++ admin-react-vite/src/views/TestDemo/TestDemo1/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Father from './components/Father';
+import Children from './components/Children';
 
 export const TestDemo1 = () => {
-    return <div>
+    return <div >
         <h1>TestDemo1</h1>
         <hr />
-        <div>
-            react 父组件调用子组件
+        <div className='my-10'> react 父组件调用子组件</div>
+        <div className="border border-[#ccc]">
+            <Father />
         </div>
     </div>
 }
