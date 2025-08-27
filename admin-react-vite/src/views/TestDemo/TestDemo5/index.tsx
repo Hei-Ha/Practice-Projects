51d428c4168e5375c0ed599f9c172ff98678b257
HeiHa <heiha.space@gmail.com>
2025-08-27 12:41:58 +0800

    封装 contentHeader 组件，添加虚拟列表

--- admin-react-vite/src/views/TestDemo/TestDemo5/index.tsx
+++ admin-react-vite/src/views/TestDemo/TestDemo5/index.tsx
@@ -1,13 +1,76 @@
-import React, { FC } from 'react';
+import React, { useState, useRef, useEffect, useCallback, FC } from 'react';
+import { ContentHeader } from '@src/components/ContentHeader';
 
 const TestDemo5: FC = () => {
-    return <div>
-        <h1>TestDemo5</h1>
-        <div className='my-10'>
-            <h2>虚拟列表</h2>
+    const arr = new Array(1000).fill(0);
+    const itemHeight = 50;
+
+    const [startIndex, setStartIndex] = useState<number>(0);
+    const [endIndex, setEndIndex] = useState<number>(0);
+
+    const listRef = useRef<HTMLDivElement | null>(null);
+
+    const scrollFun = () => {
+        if (!listRef.current) return;
+        const newStartIndex = Math.floor(listRef.current.scrollTop / itemHeight);
+        setStartIndex(newStartIndex);
+        setEndIndex(newStartIndex + Math.ceil(listRef.current.clientHeight / itemHeight));
+    };
+
+    useEffect(() => {
+        if (listRef.current) {
+            const items = Math.ceil(listRef.current.clientHeight / itemHeight);
+            setEndIndex(startIndex + items);
+            
+            listRef.current.addEventListener('scroll', scrollFun);
+        }
+        
+        return () => {
+            listRef.current?.removeEventListener('scroll', scrollFun);
+        }
+    }, []);
+
+    const renderList = useCallback(() => {
+        const rows = [];
+        for (let i = startIndex; i <= endIndex + 1; i++) {
+            // 渲染每个列表项
+            rows.push(
+                <div
+                    key={i}
+                    style={{
+                        width: '100%',
+                        height: '50px',
+                        position: 'absolute',
+                        top: i * itemHeight + 'px',
+                        left: 0,
+                        right: 0,
+                    }}
+                >
+                    {i}
+                </div>
+            );
+        }
+        return rows;
+    }, [startIndex, endIndex]);
+
+    return (
+        <div>
+            <ContentHeader title="TestDemo5" subTitle="虚拟列表" />
+            <div
+                ref={listRef}
+                className={`h-[300px] w-[500px] bg-#FFFFFF border border-solid border-#CCCCCC overflow-y-scroll text-#000000`}
+            >
+                <div
+                    style={{
+                        height: `${arr.length * itemHeight}px`,
+                        position: 'relative',
+                    }}
+                >
+                    {renderList()}
+                </div>
+            </div>
         </div>
-        <hr />
-    </div>;
+    );
 };
 
 export default TestDemo5;
